de849d99df155b676c5b5d400109df0b5c58c650
Jonathan Alvarez <j@jonalvarezz.com>
2020-06-29 17:24:48 +0200

    API: Add avo endpoints

---
+++ pages/api/avo/[id].ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import DB from '@database'
+
+const AvoDetail = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const db = new DB()
+    const avoId = req.query.id as string
+
+    const avo = await db.getById(avoId)
+
+    // Notice: We're using Next.JS response helpers here :)
+    // https://nextjs.org/docs/api-routes/response-helpers
+    res.status(200).json(avo)
+  } catch (e) {
+    console.error(e)
+    res.status(404).end()
+  }
+}
+
+export default AvoDetail
---
+++ pages/api/avo/index.ts
@@ -0,0 +1,25 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import DB from '@database'
+
+const allAvos = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    const db = new DB()
+    const allEntries = await db.getAll()
+    const lenght = allEntries.length
+
+    // Notice: We're manually setting the response object
+    // However Next.JS offers Express-like helpers :)
+    // https://nextjs.org/docs/api-routes/response-helpers
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ lenght, data: allEntries }))
+  } catch (e) {
+    console.error(e)
+    res.statusCode = 500
+    res.end(
+      JSON.stringify({ length: 0, data: [], error: 'Something went wrong' })
+    )
+  }
+}
+
+export default allAvos
