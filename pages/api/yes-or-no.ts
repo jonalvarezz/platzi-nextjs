ab87cd5140451a9babd34bc8adab347c1a8470c9
Jonathan Alvarez <j@jonalvarezz.com>
2020-06-30 13:28:52 +0200

    API yes-or-no: enable cors

--- pages/api/yes-or-no.ts
+++ pages/api/yes-or-no.ts
@@ -1,10 +1,17 @@
-import { IncomingMessage, ServerResponse } from 'http'
 import { NextApiResponse, NextApiRequest } from 'next'
+import enablePublicAccess from '@cors'
 
 const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Generally, you would not want this in your apps.
+  // See more in 'cors.js'
+  await enablePublicAccess(req, res)
+
   const answer = Math.round(Math.random()) ? 'yes' : 'no'
 
-  res.status(200).json({ data: answer, error: null })
+  res.status(200).json({
+    data: answer,
+    error: null,
+  })
 }
 
 export default allAvos
