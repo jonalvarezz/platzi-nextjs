c3e41d572e2602b351bc1d03d23d9bd0fac91409
Irving Juárez <irvingjuarez274@gmail.com>
2022-07-31 17:49:58 -0500

    More ESlint rules applied

--- pages/api/avo/[item].ts
+++ pages/api/avo/[item].ts
@@ -2,14 +2,16 @@ import DB from '@database'
 import { NextApiResponse, NextApiRequest } from 'next'
 
 const singleItem = async (req: NextApiRequest, res: NextApiResponse) => {
-  const itemId = req.query.item
+  const itemId = req.query.item as string
   const db = new DB()
 
-  const singleAvo: string = await db.getById(itemId)
+  const singleAvo: TProduct | null = await db.getById(itemId)
 
-  res.statusCode = singleAvo ? 200 : 500
-  res.setHeader('Content-type', 'application/json')
-  res.end(JSON.stringify(singleAvo))
+  if (singleAvo) {
+    res.statusCode = singleAvo ? 200 : 500
+    res.setHeader('Content-type', 'application/json')
+    res.end(JSON.stringify(singleAvo as TProduct))
+  }
 }
 
 export default singleItem
